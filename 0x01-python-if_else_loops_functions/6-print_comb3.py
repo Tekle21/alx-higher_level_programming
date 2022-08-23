6bbca09cad887b90ec5f39ada3026a04385acfdb
tekle21 <tekle1421@github.com>
2022-08-22 23:34:20 -0700

    a program that prints all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i < j:
+            print('{:d}{:d}'.format(i, j), end='')
+            if i < 8:
+                print(', ', end='')
+print()
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms

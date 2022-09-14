3aafa551a3c2c9d814b2220c9b421ab45025f78c
tekle21 <tekle1421@github.com>
2022-09-13 19:57:00 -0700

    update task 102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,87 +1,73 @@
 #!/usr/bin/python3
-"""a square class"""
+""" Creates an empty class called Square
+"""
 
 
 class Square:
-    """Derives a square """
+    """ Empty class with size private attribute
+    """
     def __init__(self, size=0):
-        """Initializes the data
-        Args:
-            size (int): size of the square
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-        Raises:
-            TypeError: when `size` isn't an integer
-            ValueError: `size` is less than 0
         """
-
-        if not isinstance(size, int):
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if (size < 0):
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def __lt__(self, other):
-        """Compare less than of current instance and other instance"""
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        """
-        Compare less than or equal to of current instance
-        and other instance
-        """
-        return self.area() <= other.area()
-
-    def __eq__(self, other):
-        """Compare equality of current instance and other instance"""
-        return self.area() == other.area()
-
-    def __ne__(self, other):
-        """
-        Compare not equal of current instance
-        and other instance
-        """
-        return self.area() != other.area()
-
-    def __gt__(self, other):
-        """
-        Compare greater than of current instance
-        and other instance
-        """
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        """
-        Compare greater than or equal to of current instance
-        and other instance
-        """
-        return self.area() >= other.area()
-
     def area(self):
-        """Calculates the area of a square
-        Returns: the area of the square
         """
-
-        return (self.__size ** 2)
+        Returns the area of the square
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        """Retrieves the value of `size`"""
+        """
+        size getter. Handle size errors
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the value of `value`
-        Args:
-            value (int): value to be set to `value`
-        Raise:
-            TypeError: when `value` isn't an integer
-            ValueError: `value` is less than 0
         """
-
-        if not isinstance(value, int):
+        size setter. Set the size square
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
-        self.__size = 
+    def __lt__(self, other):
+        """ Compare if our square is less than the other
+        """
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """ Compare if our square is less ot equal than the other
+        """
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        """ Compare if our square is equal than the other
+        """
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """ Compare if our square is different than the other
+        """
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        """ Compare if our square is greater than the other
+        """
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """ Compare if our square is less than the other
+        """
+        return self.area() >= other.area()
